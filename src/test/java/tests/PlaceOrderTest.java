a6aa842d1eb6d06c1eae005a0efd423d2f79dc5c
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-11-27 16:43:24 +0200

    Initial commit

---
+++ src/test/java/tests/PlaceOrderTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+
+import org.testng.annotations.Test;
+
+public class PlaceOrderTest extends BaseTest {
+
+    @Test
+    public void checkSiteVersion() {
+        // TODO open main page and validate website version
+    }
+
+    @Test
+    public void createNewOrder() {
+        // TODO implement order creation test
+
+        // open random product
+
+        // save product parameters
+
+        // add product to Cart and validate product information in the Cart
+
+        // proceed to order creation, fill required information
+
+        // place new order and validate order summary
+
+        // check updated In Stock value
+    }
+
+}
